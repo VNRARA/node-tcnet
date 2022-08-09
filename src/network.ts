790c0a38c9319c6c882ac0bfb4363b013e17f9ea
LTTS <l.tjoentakseu@gmail.com>
2022-08-09 17:18:28 +0200

    started adding cuedata vars

--- src/network.ts
+++ src/network.ts
@@ -748,6 +748,173 @@ export class TCNetDataPacketMixerData extends TCNetDataPacket {
     }
 }
 
+export class TCNetDataPacketCueData extends TCNetDataPacket {
+
+    loopIn: number;
+    loopOut: number;
+    cue1Type: number;
+    cue1InTime: number;
+    cue1OutTime: number;
+    cue1Color: number;
+    cue2Type: number;
+    cue2InTime: number;
+    cue2OutTime: number;
+    cue2Color: number;
+    cue3Type: number;
+    cue3InTime: number;
+    cue3OutTime: number;
+    cue3Color: number;
+    cue4Type: number;
+    cue4InTime: number;
+    cue4OutTime: number;
+    cue4Color: number;
+    cue5Type: number;
+    cue5InTime: number;
+    cue5OutTime: number;
+    cue5Color: number;
+    cue6Type: number;
+    cue6InTime: number;
+    cue6OutTime: number;
+    cue6Color: number;
+    cue7Type: number;
+    cue7InTime: number;
+    cue7OutTime: number;
+    cue7Color: number;
+    cue8Type: number;
+    cue8InTime: number;
+    cue8OutTime: number;
+    cue8Color: number;
+    cue9Type: number;
+    cue9InTime: number;
+    cue9OutTime: number;
+    cue9Color: number;
+    cue10Type: number;
+    cue10InTime: number;
+    cue10OutTime: number;
+    cue10Color: number;
+    cue11Type: number;
+    cue11InTime: number;
+    cue11OutTime: number;
+    cue11Color: number;
+    cue12Type: number;
+    cue12InTime: number;
+    cue12OutTime: number;
+    cue12Color: number;
+    cue13Type: number;
+    cue13InTime: number;
+    cue13OutTime: number;
+    cue13Color: number;
+    cue14Type: number;
+    cue14InTime: number;
+    cue14OutTime: number;
+    cue14Color: number;
+    cue15Type: number;
+    cue15InTime: number;
+    cue15OutTime: number;
+    cue15Color: number;
+    cue16Type: number;
+    cue16InTime: number;
+    cue16OutTime: number;
+    cue16Color: number;
+    cue17Type: number;
+    cue17InTime: number;
+    cue17OutTime: number;
+    cue17Color: number;
+    cue18Type: number;
+    cue18InTime: number;
+    cue18OutTime: number;
+    cue18Color: number;
+
+    read(): void {
+        this.loopIn = this.buffer.readUInt32LE(42);
+        this.loopOut = this.buffer.readUInt32LE(46);
+
+        this.cue1Type = this.buffer.readUInt8(47);
+        this.cue1InTime = this.buffer.readUInt32LE(49);
+        this.cue1OutTime = this.buffer.readUInt32LE(53);
+        //this.cue1Color = this.toColor(this.buffer.subarray(58, 60)); // 3 byte value
+
+        this.cue2Type = this.buffer.readUInt32LE(32);
+        this.cue2InTime = this.buffer.readUInt32LE(32);
+        this.cue2OutTime = this.buffer.readUInt32LE(32);
+        this.cue2Color = this.buffer.readUInt32LE(32);
+        this.cue3Type = this.buffer.readUInt32LE(32);
+        this.cue3InTime = this.buffer.readUInt32LE(32);
+        this.cue3OutTime = this.buffer.readUInt32LE(32);
+        this.cue3Color = this.buffer.readUInt32LE(32);
+        this.cue4Type = this.buffer.readUInt32LE(32);
+        this.cue4InTime = this.buffer.readUInt32LE(32);
+        this.cue4OutTime = this.buffer.readUInt32LE(32);
+        this.cue4Color = this.buffer.readUInt32LE(32);
+        this.cue5Type = this.buffer.readUInt32LE(32);
+        this.cue5InTime = this.buffer.readUInt32LE(32);
+        this.cue5OutTime = this.buffer.readUInt32LE(32);
+        this.cue5Color = this.buffer.readUInt32LE(32);
+        this.cue6Type = this.buffer.readUInt32LE(32);
+        this.cue6InTime = this.buffer.readUInt32LE(32);
+        this.cue6OutTime = this.buffer.readUInt32LE(32);
+        this.cue6Color = this.buffer.readUInt32LE(32);
+        this.cue7Type = this.buffer.readUInt32LE(32);
+        this.cue7InTime = this.buffer.readUInt32LE(32);
+        this.cue7OutTime = this.buffer.readUInt32LE(32);
+        this.cue7Color = this.buffer.readUInt32LE(32);
+        this.cue8Type = this.buffer.readUInt32LE(32);
+        this.cue8InTime = this.buffer.readUInt32LE(32);
+        this.cue8OutTime = this.buffer.readUInt32LE(32);
+        this.cue8Color = this.buffer.readUInt32LE(32);
+        this.cue9Type = this.buffer.readUInt32LE(32);
+        this.cue9InTime = this.buffer.readUInt32LE(32);
+        this.cue9OutTime = this.buffer.readUInt32LE(32);
+        this.cue9Color = this.buffer.readUInt32LE(32);
+        this.cue10Type = this.buffer.readUInt32LE(32);
+        this.cue10InTime = this.buffer.readUInt32LE(32);
+        this.cue10OutTime = this.buffer.readUInt32LE(32);
+        this.cue10Color = this.buffer.readUInt32LE(32);
+        this.cue11Type = this.buffer.readUInt32LE(32);
+        this.cue11InTime = this.buffer.readUInt32LE(32);
+        this.cue11OutTime = this.buffer.readUInt32LE(32);
+        this.cue11Color = this.buffer.readUInt32LE(32);
+        this.cue12Type = this.buffer.readUInt32LE(32);
+        this.cue12InTime = this.buffer.readUInt32LE(32);
+        this.cue12OutTime = this.buffer.readUInt32LE(32);
+        this.cue12Color = this.buffer.readUInt32LE(32);
+        this.cue13Type = this.buffer.readUInt32LE(32);
+        this.cue13InTime = this.buffer.readUInt32LE(32);
+        this.cue13OutTime = this.buffer.readUInt32LE(32);
+        this.cue13Color = this.buffer.readUInt32LE(32);
+        this.cue14Type = this.buffer.readUInt32LE(32);
+        this.cue14InTime = this.buffer.readUInt32LE(32);
+        this.cue14OutTime = this.buffer.readUInt32LE(32);
+        this.cue14Color = this.buffer.readUInt32LE(32);
+        this.cue15Type = this.buffer.readUInt32LE(32);
+        this.cue15InTime = this.buffer.readUInt32LE(32);
+        this.cue15OutTime = this.buffer.readUInt32LE(32);
+        this.cue15Color = this.buffer.readUInt32LE(32);
+        this.cue16Type = this.buffer.readUInt32LE(32);
+        this.cue16InTime = this.buffer.readUInt32LE(32);
+        this.cue16OutTime = this.buffer.readUInt32LE(32);
+        this.cue16Color = this.buffer.readUInt32LE(32);
+        this.cue17Type = this.buffer.readUInt32LE(32);
+        this.cue17InTime = this.buffer.readUInt32LE(32);
+        this.cue17OutTime = this.buffer.readUInt32LE(32);
+        this.cue17Color = this.buffer.readUInt32LE(32);
+        this.cue18Type = this.buffer.readUInt32LE(32);
+        this.cue18InTime = this.buffer.readUInt32LE(32);
+        this.cue18OutTime = this.buffer.readUInt32LE(32);
+        this.cue18Color = this.buffer.readUInt32LE(32);
+
+    }
+
+    write(): void {
+        throw new Error("not supported!");
+    }
+    length(): number {
+        return 270;
+    }
+
+}
+
+
 
 
 export interface Constructable {
@@ -773,9 +940,9 @@ export const TCNetPackets: Record<TCNetMessageType, Constructable | null> = {
 export const TCNetDataPackets: Record<TCNetDataPacketType, typeof TCNetDataPacket | null> = {
     [TCNetDataPacketType.MetricsData]: TCNetDataPacketMetrics,
     [TCNetDataPacketType.MetaData]: TCNetDataPacketMetadata,
-    [TCNetDataPacketType.BeatGridData]: TCNetDataPacketBeatGridData, // not yet implemented
-    [TCNetDataPacketType.CUEData]: null, // not yet implemented
+    [TCNetDataPacketType.BeatGridData]: TCNetDataPacketBeatGridData, // does not work yet
+    [TCNetDataPacketType.CUEData]: TCNetDataPacketCueData, // not yet implemented
     [TCNetDataPacketType.SmallWaveFormData]: null, // not yet implemented
     [TCNetDataPacketType.BigWaveFormData]: null, // not yet implemented
-    [TCNetDataPacketType.MixerData]: TCNetDataPacketMixerData, // not yet implemented
+    [TCNetDataPacketType.MixerData]: TCNetDataPacketMixerData, // seems to give mostly 0
 };
